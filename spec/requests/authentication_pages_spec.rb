61e62c69e576df15704713cbdc0f8296f32004db
Bunyod Avliyokulov <bunyod80@gmail.com>
2014-09-08 23:26:14 +0500

    Chapter 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,39 +3,41 @@ require 'spec_helper'
 describe "Authentication" do
 	subject { page }
 
-	describe "signin" do
+  describe "signin" do
 		before { visit signin_path }
 
 		describe "with invalid information" do
-      		before { click_button "Sign in" }
+      before { click_button "Sign in" }
 
-      		it { should have_title('Sign in') }
-      		it { should have_selector('div.alert.alert-error') }
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
 
-      		describe "after visiting another page" do
-  				before { click_link "Home" }
-  				it { should_not have_selector('div.alert.alert-error') }
-			end
-    	end
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
 
-    	describe "with valid information" do
-    		let(:user) { FactoryGirl.create(:user) }
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
 
-      		before { sign_in user }
-      		 
-    		it { should have_title(user.name) }
-        it { should have_link('Users', href: users_path) }
-    		it { should have_link('Profile', href: user_path(user)) }
-        it { should have_link('Settings', href: edit_user_path(user)) }
-    		it { should have_link('Sign out', href: signout_path) }
-    		it { should_not have_link('Sign in', href: signin_path) }
+      before { sign_in user }
 
-        describe "followed by signout" do
-          before { click_link "Sign out"}
-          it { should have_link('Sign in')}
-        end
-    	end
-	end
+      it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out"}
+        it { should have_link('Sign in')}
+        it { should_not have_link('Profile', href: user_path(user)) }
+        it { should_not have_link('Settings', href: edit_user_path(user)) }
+      end
+    end
+  end
 
   describe "authorization" do
 
@@ -45,9 +47,7 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          valid_signin user
         end
 
         describe "after signing in" do
@@ -102,6 +102,24 @@ describe "Authentication" do
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
         specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user, no_capybara: true }
+
+      describe "using a 'new' action" do
+        before { get signup_path }
+        specify { response.should redirect_to(root_url) }
+      end
+
+      describe "using a 'create' action" do
+        let(:params) do
+          { user: {} }
+        end 
+        before { post users_path(user), params }
+        specify { response.should redirect_to(root_url) }
       end
     end
   end
